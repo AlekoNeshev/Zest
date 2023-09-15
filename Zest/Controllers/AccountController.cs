aa3a00c4f426702e323d672af7053be3f02a1b08
Aleko Neshev <alekonesh@gmail.com>
2023-09-15 20:32:18 +0300

    Account Controller

---
+++ Zest/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Zest.DBModels;
+using Zest.DBModels.Models;
+
+namespace Zest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AccountController : ControllerBase
+    {
+        public ZestContext zestContext;
+        public AccountController(ZestContext zestContext)
+        {
+            this.zestContext = zestContext;
+        }
+        [HttpGet]
+        public async Task<ActionResult<Account>> Index(int id)
+        {
+            return zestContext.Accounts.Where(x=>x.Id ==id).FirstOrDefault();
+        }
+    }
+}
