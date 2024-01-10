b36b213634ed43058abb98d4bb4d89ae21b28c4e
Aleko Neshev <alekonesh@gmail.com>
2024-01-10 14:45:06 +0200

    File upload and get

--- Zest/Controllers/PostRescourcesController.cs
+++ Zest/Controllers/PostRescourcesController.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+using System.Text.RegularExpressions;
 using Zest.DBModels;
 using Zest.DBModels.Models;
 using Zest.ViewModels.ViewModels;
@@ -34,6 +36,64 @@ namespace Zest.Controllers
 			await context.SaveChangesAsync();
 			return Ok();
 		}
-		
+
+		[HttpPost("ivan")]
+		public async Task<IActionResult> UploadFile(IFormFile postedFile)
+		{
+			//if (!string.Equals(postedFile.ContentType, "image/jpg", StringComparison.OrdinalIgnoreCase) &&
+			//   !string.Equals(postedFile.ContentType, "image/jpeg", StringComparison.OrdinalIgnoreCase) &&
+			//   !string.Equals(postedFile.ContentType, "image/pjpeg", StringComparison.OrdinalIgnoreCase) &&
+			//   !string.Equals(postedFile.ContentType, "image/gif", StringComparison.OrdinalIgnoreCase) &&
+			//   !string.Equals(postedFile.ContentType, "image/x-png", StringComparison.OrdinalIgnoreCase) &&
+			//   !string.Equals(postedFile.ContentType, "image/png", StringComparison.OrdinalIgnoreCase))
+			//{
+			//	return Problem();
+			//}
+
+			////-------------------------------------------
+			////  Check the image extension
+			////-------------------------------------------
+			var postedFileExtension = Path.GetExtension(postedFile.FileName);
+			//if (!string.Equals(postedFileExtension, ".jpg", StringComparison.OrdinalIgnoreCase)
+			//	&& !string.Equals(postedFileExtension, ".png", StringComparison.OrdinalIgnoreCase)
+			//	&& !string.Equals(postedFileExtension, ".gif", StringComparison.OrdinalIgnoreCase)
+			//	&& !string.Equals(postedFileExtension, ".jpeg", StringComparison.OrdinalIgnoreCase))
+			//{
+			//	return Problem();
+			//}
+
+			string uploads = Path.Combine(Assembly.GetEntryAssembly().Location.Replace("Zest.dll", ""), "uploads");
+			if (!Directory.Exists(uploads)) Directory.CreateDirectory(uploads);
+			
+			var un = Guid.NewGuid().ToString();
+
+			if (postedFile.Length > 0)
+			{
+				string filePath = Path.Combine(uploads, $"{un}{postedFileExtension}");
+				using (Stream fileStream = new FileStream(filePath, FileMode.Create))
+				{
+					await postedFile.CopyToAsync(fileStream);
+				}
+			}
+
+			return Ok($"{un}{postedFileExtension}");
+		}
+
+		[HttpGet("ivan/{fileName}")]
+		public async Task<FileResult> GetFile([FromQuery] string fileName)
+		{
+			string uploads = Path.Combine(Assembly.GetEntryAssembly().Location.Replace("Zest.dll", ""), "uploads");
+			return File(System.IO.File.OpenRead(Path.Combine(uploads, fileName)), GetMimeTypeByWindowsRegistry(fileName.Split(".").Last()));
+		}
+
+		public static string GetMimeTypeByWindowsRegistry(string fileNameOrExtension)
+		{
+			string mimeType = "application/unknown";
+			string ext = (fileNameOrExtension.Contains(".")) ? System.IO.Path.GetExtension(fileNameOrExtension).ToLower() : "." + fileNameOrExtension;
+			Microsoft.Win32.RegistryKey regKey = Microsoft.Win32.Registry.ClassesRoot.OpenSubKey(ext);
+			if (regKey != null && regKey.GetValue("Content Type") != null) mimeType = regKey.GetValue("Content Type").ToString();
+			return mimeType;
+		}
+
 	}
 }
